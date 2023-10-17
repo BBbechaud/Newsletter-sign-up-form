e850e3b5bc69603bf1a93ed0212a5fb8f8454015
BBbechaud <132162306+BBbechaud@users.noreply.github.com>
2023-10-17 13:12:33 -0400

    functionality added to the dismis btn

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 const form = document.querySelector('form')
 const main_container = document.querySelector('.main-container')
 const subscribed_container = document.querySelector('.subscribed-container')
+const dismiss = document.querySelector('.dismiss')
+const input = document.querySelector('input')
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     main_container.classList.add('hide')
     subscribed_container.classList.remove('hide')
+})
+
+dismiss.addEventListener('click', (e) => {
+    e.preventDefault()
+    main_container.classList.toggle('hide')
+    subscribed_container.classList.toggle('hide')
+    
+    input.value = ''
 })
 
